a61ec06efc90f49ffa66684a8a11b32c14c6b83b
Mark Danovich <mark.danovich@gmail.com>
2016-01-22 11:43:13 +0000

    marking searched keywords in results

--- arxiv.php
+++ arxiv.php
@@ -20,25 +20,29 @@ function prb(){
 }
 
 
-#prb()
-
+#get keywords string from the search
 $keys=$_POST["keys"];
+
+#get or/and user selectrion
 $orand=$_POST["orand"];
+
+#convert keys to array
 $arr=explode(",",$keys);
 $query="";
 
 $size=sizeof($arr);
 $i=0;
 
+#prepare query
 if ($orand==0){
 foreach ($arr as $el){
-	$i=$i+1;
-	if ($el!="") {
-		$query = $query.$el;
-	}
-	if ($i<=$size-2) {
-		  $query=$query."+OR+";
-	}
+    $i=$i+1;
+    if ($el!="") {
+        $query = $query.$el;
+    }
+    if ($i<=$size-2) {
+          $query=$query."+OR+";
+    }
 }
 }else{
 foreach ($arr as $el){
@@ -52,14 +56,18 @@ foreach ($arr as $el){
 }
 }
 
+#number of returned papers is limited to 20
 $return="";
 $limit=20;
 
+#arxiv api
 $url = 'http://export.arxiv.org/api/query?search_query=all:'.$query.'&sortBy=submittedDate&start=0&max_results='.$limit;
 
+#the response is casted into an xml object
 $response = file_get_contents($url);
 $xml = new SimpleXMLElement($response);
 
+#read information from the xml response
 foreach ($xml->entry as $entry){
     $link=$entry->id;
     $start = strpos($link,'abs');
@@ -74,13 +82,29 @@ foreach ($xml->entry as $entry){
         $authors .=$author->name.", ";
     }
     $authors = substr($authors,0,strlen($authors)-2);
+
+    $newtitle=$title;
+    $newabstract=$abstract;
+    #mark the keywords in the title/abstract with a yellow background
+    foreach ($arr as $el){
+        $rep = "<span style='background-color:yellow;'>".$el."</span> ";
+        if (strpos($el,strtolower($title))!=-1) {
+            $newtitle = str_replace($el,$rep,strtolower($newtitle));
+        }
+        if (strpos($el,strtolower($abstract))!=-1) {
+            $rep = "<span style='background-color:yellow;'>".$el."</span> ";
+            $newabstract = str_replace($el,$rep,strtolower($newabstract));
+        }
+    }
+
+    #create paper item
     $item='<div class="list-group">
             <div>
                 <h6 style="float:right; margin-top:0px;">'.$date.'</h6>
-                <h4 class="list-group-item-heading" style="width:87%;"><a href="'.$link.'" target="_blank">'.$title.'</a>
+                <h4 class="list-group-item-heading" style="width:87%;"><a href="'.$link.'" target="_blank">'.$newtitle.'</a>
                  <a href="'.$linkpdf.'" target="_blank">[PDF]</a></h4>
                 <h5>'.$authors.'</h6>
-                <p class="list-group-item-text">'.$abstract.'</p>
+                <p class="list-group-item-text">'.$newabstract.'</p>
             </div>
            </div>';
     $return = $return.$item;
